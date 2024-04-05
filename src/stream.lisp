0fbe98468400b03053278e3f8ece8c4b78756158
Masaya Tojo <masaya@tojo.tokyo>
2024-04-06 02:30:55 +0900

    Update stream interface

--- src/stream.lisp
+++ src/stream.lisp
@@ -4,9 +4,13 @@
   (:shadow #:cons
            #:append
            #:take
+           #:drop
            #:filter
-           #:any
+           #:index
+           #:head
+           #:tail
            #:repeat
+           #:length
            #:concat)
   (:local-nicknames
    (#:promise #:tokyo.tojo.lazy/promise))
@@ -16,13 +20,20 @@
            #:force
            #:delay-force
            #:null
+           #:head
+           #:tail
+           #:index
            #:append
            #:concat
            #:concat-map
-           #:zip-with
            #:take
-           #:repeat
+           #:drop
+           #:take-while
            #:drop-while
+           #:zip-with
+           #:iterate
+           #:repeat
+           #:length
            #:filter))
 
 (in-package #:tokyo.tojo.lazy/stream)
@@ -52,21 +63,34 @@
     (%Nil)
     (%Cons :a (Stream :a)))
 
-  (declare null (Stream :a))
-  (define null
-    (%Stream (promise:delay None)))
-
   (declare force (Stream :a -> Optional (Tuple :a (Stream :a))))
   (define (force (%Stream p))
     (promise:force p))
 
-  (define-instance (Functor Stream)
-    (define (map f x)
-      (delay-force
-       (match (force x)
-         ((None) null)
-         ((Some (Tuple h t))
-          (cons (f h) (map f t)))))))
+  (declare null (Stream :a))
+  (define null
+    (%Stream (promise:delay None)))
+
+  (declare head (Stream :a -> Optional :a))
+  (define (head s)
+    (match (force s)
+      ((Some (Tuple h _)) (Some h))
+      ((None) None)))
+
+  (declare tail (Stream :a -> Optional (Stream :a)))
+  (define (tail s)
+    (match (force s)
+      ((Some (Tuple _ t)) (Some t))
+      ((None) None)))
+
+  (declare index (UFix -> Stream :a -> Optional :a))
+  (define (index idx s)
+    (match (force s)
+      ((Some (Tuple h t))
+       (if (== idx 0)
+           (Some h)
+           (index (1- idx) t)))
+      ((None) None)))
 
   (declare append (Stream :a -> Stream :a -> Stream :a))
   (define (append s1 s2)
@@ -88,6 +112,115 @@
        ((Some (Tuple h t))
         (append (f h) (concat-map f t))))))
 
+  (declare take (UFix -> (Stream :a) -> (Stream :a)))
+  (define (take n s)
+    (delay-force
+     (if (<= n 0)
+         null
+         (match (force s)
+           ((None) null)
+           ((Some (Tuple h t))
+            (cons h (take (1- n) t)))))))
+
+  (declare drop (UFix -> (Stream :a) -> (Stream :a)))
+  (define (drop n s)
+    (delay-force
+     (if (<= n 0)
+         s
+         (match (force s)
+           ((None) null)
+           ((Some (Tuple _h t))
+            (drop (1- n) t))))))
+
+  (declare take-while ((:a -> Boolean) -> (Stream :a) -> (Stream :a)))
+  (define (take-while p? s)
+    (delay-force
+     (match (force s)
+       ((None) null)
+       ((Some (Tuple h t))
+        (if (p? h)
+            (cons h (take-while p? t))
+            null)))))
+
+  (declare drop-while ((:a -> Boolean) -> (Stream :a) -> (Stream :a)))
+  (define (drop-while p? s)
+    (delay-force
+     (match (force s)
+       ((None) null)
+       ((Some (Tuple h t))
+        (if (p? h)
+            (drop-while p? t)
+            s)))))
+
+  (declare zip-with ((:a -> :b -> :c) -> (Stream :a) -> (Stream :b) -> (Stream :c)))
+  (define (zip-with f s1 s2)
+    (delay-force
+     (match (force s1)
+       ((None) null)
+       ((Some (Tuple h1 t1))
+        (match (force s2)
+          ((None) null)
+          ((Some (Tuple h2 t2))
+           (cons (f h1 h2)
+                 (zip-with f t1 t2))))))))
+
+  (declare iterate ((:a -> :a) -> :a -> (Stream :a)))
+  (define (iterate f x)
+    (cons x (iterate f (f x))))
+
+  (declare repeat (:a -> (Stream :a)))
+  (define repeat (iterate id))
+
+  (declare length (Stream :a -> UFix))
+  (define (length s)
+    (fold (fn (acc _) (1+ acc)) 0 s))
+
+  (declare filter ((:a -> Boolean) -> (Stream :a) -> (Stream :a)))
+  (define (filter p? s)
+    (delay-force
+     (match (force s)
+       ((None) null)
+       ((Some (Tuple h t))
+        (if (p? h)
+            (cons h (filter p? t))
+            (filter p? t))))))
+
+  (define-instance (Eq :a => Eq (Stream :a))
+    (define (== s1 s2)
+      (match (force s1)
+        ((Some (Tuple h1 t1))
+         (match (force s2)
+           ((Some (Tuple h2 t2))
+            (and (== h1 h2)
+                 (== t1 t2)))
+           (_ False)))
+        ((None)
+         (match (force s2)
+           ((None) True)
+           (_ False))))))
+
+  (define-instance (Eq :a => Eq (Stream :a))
+    (define (== s1 s2)
+      (match (force s1)
+        ((Some (Tuple h1 t1))
+         (match (force s2)
+           ((Some (Tuple h2 t2))
+            (and (== h1 h2)
+                 (== t1 t2)))
+           (_ False)))
+        ((None)
+         (match (force s2)
+           ((None) True)
+           (_ False))))))
+
+  (define-instance (Functor Stream)
+    (define (map f x)
+      (delay-force
+       (match (force x)
+         ((None) null)
+         ((Some (Tuple h t))
+          (cons (f h) (map f t)))))))
+
   (define-instance (Applicative Stream)
     (define (pure x) (make x))
     (define (liftA2 op s1 s2)
@@ -103,15 +236,17 @@
       (append s1 s2))
     (define empty null))
 
-  (declare take (Integer -> (Stream :a) -> (Stream :a)))
-  (define (take n s)
-    (delay-force
-     (if (<= n 0)
-         null
-         (match (force s)
-           ((None) null)
-           ((Some (Tuple h t))
-            (cons h (take (1- n) t)))))))
+  (define-instance (Foldable Stream)
+    (define (fold f init s)
+      (match (force s)
+        ((None) init)
+        ((Some (Tuple h t))
+         (fold f (f init h) t))))
+    (define (foldr f init s)
+      (match (force s)
+        ((None) init)
+        ((Some (Tuple h t))
+         (f h (foldr f init t))))))
 
   (define-instance (Into (Stream :a) (List :a))
     (define (into s)
@@ -125,44 +260,4 @@
       (match lst
         ((Nil) null)
         ((coalton:Cons h t)
-         (cons h (into t))))))
-
-  (declare zip-with ((:a -> :b -> :c) -> (Stream :a) -> (Stream :b) -> (Stream :c)))
-  (define (zip-with f s1 s2)
-    (delay-force
-     (match (force s1)
-       ((None) null)
-       ((Some (Tuple h1 t1))
-        (match (force s2)
-          ((None) null)
-          ((Some (Tuple h2 t2))
-           (cons (f h1 h2)
-                 (zip-with f t1 t2))))))))
-
-  (declare repeat (:a -> (Stream :a)))
-  (define (repeat x)
-    (cons x (repeat x)))
-
-  (declare naturals-from (Integer -> (Stream Integer)))
-  (define (naturals-from n)
-    (cons n (naturals-from (+ n 1))))
-
-  (declare drop-while ((:a -> Boolean) -> (Stream :a) -> (Stream :a)))
-  (define (drop-while p? s)
-    (delay-force
-     (match (force s)
-       ((None) null)
-       ((Some (Tuple h t))
-        (if (p? h)
-            (drop-while p? t)
-            s)))))
-
-  (declare filter ((:a -> Boolean) -> (Stream :a) -> (Stream :a)))
-  (define (filter p? s)
-    (delay-force
-     (match (force s)
-       ((None) null)
-       ((Some (Tuple h t))
-        (if (p? h)
-            (cons h (filter p? t))
-            (filter p? t)))))))
+         (cons h (into t)))))))
